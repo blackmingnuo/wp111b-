e708f653f80bff63ef8d918e58a63fe30ca57994
blackmingnuo <jasone0528@gmail.com>
2023-03-31 10:37:44 +0800

    add js1-1.js

---
+++ homework 4/js1-1.js
@@ -0,0 +1,17 @@
+function isPrime(n)
+{   
+    if(n<=1)
+    {
+        return false;
+    }
+    for(var i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+    
+}
+console.log(isPrime(3));
---
+++ homework 4/js1-3.js
@@ -0,0 +1,4 @@
+function truthTable(n)
+{
+    
+}
